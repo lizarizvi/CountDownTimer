251f792f151a5b5e5759c6c0381885d2e6358995
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-12 13:09:05 +0530

    viewBinding and setting Toast to Buttons

--- app/src/main/java/com/example/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.countdowntimer
 
+import android.content.IntentSender.OnFinished
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import com.example.countdowntimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var countDownTimer : CountDownTimer? = null
+    private var timeDuration : Long = 60000 //in ms
+    private var pauseOffset : Long = 0 //pauseOffset=timeDuration-timeLeft
+
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.tvTimer?.text = "${(timeDuration/1000).toString()}"
+        binding?.startBtn?.setOnClickListener {
+            Toast.makeText(this@MainActivity,"timer started", Toast.LENGTH_SHORT).show()
+        }
+        binding?.pauseBtn?.setOnClickListener {
+            Toast.makeText(this@MainActivity,"timer paused",Toast.LENGTH_SHORT).show()
+        }
+        binding?.resetBtn?.setOnClickListener {
+            Toast.makeText(this@MainActivity,"reset timer",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
